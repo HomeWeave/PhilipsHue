3dc99b0ba803ff67dd9b9d4f8a032b68cbce3c18
Srivatsan Iyer <supersaiyanmode.rox@gmail.com>
2021-05-07 01:36:04 +0530

    Remove old code.

--- weavehue/__init__.py
+++
@@ -1,9 +0,0 @@
-from .service import HueService
-
-
-__meta__ = {
-    "name": "Philips Hue Plugin",
-    "class": HueService,
-    "deps": ["messaging", "appmanager", "simpledb"],
-    "config": [],
-}
